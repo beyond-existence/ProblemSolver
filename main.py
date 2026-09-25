f8d7cc5abc9672adf2bb3773559efdc04360f3e0
Arnab Choudhury <arnab2011choudhury@gmail.com>
2026-09-25 21:41:30 +0530

    Updated the final loop and an advanced image system

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from ollama import Client
 from pydantic import BaseModel
+from tkinter import filedialog
+
+Image_input = None
+
+def get_image():
+    global Image_input
+    Image_input = filedialog.askopenfilename(
+        title="Select Image",
+        filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")]
+    )
+
 
 client = Client(
     host= "https://ollama.com",
@@ -17,43 +28,45 @@ CRITICAL REQUIREMENT: You must respond ONLY with a raw JSON object matching this
   "safety": "string description"
 }"""
 
+while True:
+    CONTEXT = input("Enter your problem (or q to exit): ")
+    if CONTEXT == 'q':
+        break
+    get_image()
+    path = Image_input
 
-CONTEXT = input("Enter your problem: ")
-path = r"E:\Problem_solver\problem.jpg"
+    class ProblemSolver(BaseModel):
+        problem: str
+        possible_cause: str
+        action: list[str]
+        safety: str
 
-class ProblemSolver(BaseModel):
-    problem: str
-    possible_cause: str
-    action: list[str]
-    safety: str
+    response = client.chat(
+        model='gemma4:31b-cloud',
+        format=ProblemSolver.model_json_schema(),
+        messages=[
+            {
+                'role' : 'system',
+                'content' : SYSTEM_PROMPT,
+            },
+            {
+                'role' : 'user',
+                'content' : CONTEXT,
+                'images' : [path],
+            }
+        ],
+        options={'temperature' : 0}
+    )
 
-response = client.chat(
-    model='gemma4:31b-cloud',
-    format=ProblemSolver.model_json_schema(),
-    messages=[
-        {
-            'role' : 'system',
-            'content' : SYSTEM_PROMPT,
-        },
-        {
-            'role' : 'user',
-            'content' : CONTEXT,
-            'images' : [path],
-        }
-    ],
-    options={'temperature' : 0}
-)
+    Solved = ProblemSolver.model_validate_json(response['message']['content'])
 
-Solved = ProblemSolver.model_validate_json(response['message']['content'])
+    print("\nImage Analysis Results:")
+    print("-" * 50)
+    print(f"\nProblem:\n{Solved.problem}")
+    print(f"\npossible_cause: {Solved.possible_cause}")
+    print("\nActions which can be taken:")
+    for obj in Solved.action:
+        print(obj)
+    print(f"\nSafety:\n{Solved.safety}")
 
-print("\nImage Analysis Results:")
-print("-" * 50)
-print(f"\nProblem:\n{Solved.problem}")
-print(f"\npossible_cause: {Solved.possible_cause}")
-print("\nActions which can be taken:")
-for obj in Solved.action:
-    print(obj)
-print(f"\nSafety:\n{Solved.safety}")
-
-print("HI:)")
 #Why is the circuit not working?
